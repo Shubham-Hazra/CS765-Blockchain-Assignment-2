94d4e78f3a964876e1ad5a8f68bd6069b0482962
Shubham-Hazra <shubhamhazra1234@gmail.com>
2023-03-17 19:38:13 +0530

    code done

--- src/main.py
+++ src/main.py
@@ -11,8 +11,8 @@ cli.add_argument("-n","--nodes", type=int, default=25, help="Number of nodes") #
 cli.add_argument("-z","--zeta", type=int, default=50, help="Percentage of nodes the adversary is connected to") # Percentage of nodes the adversary is connected to
 cli.add_argument("-z0","--low_cpu", type=float, default=0, help="Percentage of slow nodes") # Percentage of slow nodes
 cli.add_argument("-z1","--low_speed", type=float, default=0, help="Percentage of low CPU nodes") # Percentage of low CPU nodes
-cli.add_argument("--Ttx", type=float, default=10, help="Mean transaction interarrival time") # Mean transaction interarrival time
-cli.add_argument("--I", type=float, default=4, help="Mean block interarrival time") # Mean block interarrival time
+cli.add_argument("-T","--Ttx", type=float, default=10, help="Mean transaction interarrival time") # Mean transaction interarrival time
+cli.add_argument("-I","--interarrival_block", type=float, default=4, help="Mean block interarrival time") # Mean block interarrival time
 cli.add_argument("-t","--time", type=int, default=1000, help="The amount of time to run the simulation for") # The number of steps to run the simulation for
 cli.add_argument("--type",type = int , default = 0, help = "Type of simulation to run. 0 for normal run, 1 for selfish mining and 2 for stubborn mining") # Type of simulation to run
 cli.add_argument("-v","--visualize", action='store_true', help="Visualize the blockchain tree") # Visualize the blockchain tree
@@ -20,14 +20,14 @@ cli.add_argument("-d","--dump", action='store_true', help="Dump the blockchain t
 cli.add_argument("--normal", action='store_true', help="Run the simulation with normal nodes") # Run the simulation with normal nodes
 cli.add_argument("-s","--show_progress", action='store_true', help="Show the progression with time")
 cli.add_argument("-p","--print", action='store_true', help="Prints the output to the stdout")
-cli.add_argument("-ah","--adversary_hashing", type=int, default=-1, help="The hashing of the adversary") # The hashing of the adversary
+cli.add_argument("-a","--adversary_hashing", type=int, default=-1, help="The hashing of the adversary") # The hashing of the adversary
 
 args = cli.parse_args() # Parse the arguments
 
 simulation_type = args.type
 
 def main():
-    simulator = Simulator(args.nodes,args.zeta,args.low_cpu,args.low_speed, args.Ttx, args.I, args.time, simulation_type ,args.print, args.adversary_hashing)
+    simulator = Simulator(args.nodes,args.zeta,args.low_cpu,args.low_speed, args.Ttx, args.interarrival_block, args.time, simulation_type ,args.print, args.adversary_hashing)
     simulator.run()
     folders = os.listdir()
     if args.visualize:
